afcb0f16c8966a208deddb4b76579cc1d6fb58ff
frontierDelta <54361730+PradeepOff@users.noreply.github.com>
2019-12-24 04:26:23 -0800

    Part 3

---
+++ user/urls.py
@@ -0,0 +1,35 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('', views.home,name="main"),
+    path('profile/', views.profile, name='profile'),
+    path('about/',views.about,name="about"),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('register/', views.register, name='register'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('password-reset/',
+    	auth_views.PasswordResetView.as_view(
+             template_name='user/password_reset.html'
+         ),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='user/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='user/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+    path('password-reset-complete/',
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='user/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
+    # path('update_progress/',views.update_progress,name='update_progress'),
+]
---
+++ user/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegistrationForm,UserUpdateForm,ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
+from book.models import Book
+from django.views.generic import ListView
+from django.db.models import Q
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Your Account has been created successfully!')
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request,'user/registration.html',{'form':form})
+
+def login(request):
+    return render(request,'user/login.html',{'form':form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request,f'Your Account has been updated successfully!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+        
+    context = {
+        'u_form':u_form,
+        'p_form':p_form
+    }
+    return render(request,'user/profile.html',context)
+
+def logout(request):
+    return render(request,'user/logout.html')
+
+def home(request):
+    return render(request,'user/main.html')
+
+def about(request):
+    return render(request,'user/about.html')
