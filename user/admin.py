6b1881994d856d7cdae6d4d857b1d96beb5b5065
frontierDelta <54361730+PradeepOff@users.noreply.github.com>
2020-01-15 20:31:12 -0800

    created comments model

--- user/admin.py
+++ user/admin.py
@@ -5,3 +5,11 @@ from .models import *
 admin.site.register(Profile)
 admin.site.register(Collection)
 admin.site.register(Status)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
